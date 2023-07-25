14071c2ab7d37100233905ee2c950617f1897138
viniciusmontes <viniciusmontes@outlook.com.br>
2023-07-25 19:57:25 -0300

    Selecting form elements

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,12 +1,29 @@
+function State() {
+  this.btnSave = null;
+  this.btnClear = null;
 
+  this.inputCep = null;
+  this.inputStreet = null;
+  this.inputNumber = null;
+  this.inputCity = null;
 
-
-export function init() {
-
-    const inputCep = document.forms.newAddress.cep;
-
-    console.log(inputCep);
-
-
+  this.errorCep = null;
+  this.errorNumber = null;
 }
 
+const state = new State();
+
+export function init() {
+  state.inputCep = document.forms.newAddress.cep;
+  state.inputStreet = document.forms.newAddress.street;
+  state.inputNumber = document.forms.newAddress.number;
+  state.inputCity = document.forms.newAddress.city;
+
+  state.btnSave = document.forms.newAddress.btnSave;
+  state.btnClear = document.forms.newAddress.btnClear;
+
+  state.errorCep = document.querySelector('[data-error="cep"]');
+  state.errorNumber = document.querySelector('[data-error="number"]');
+
+  console.log(state);
+}
