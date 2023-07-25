2df1e93da91f5514804b82278da799333d08872d
viniciusmontes <viniciusmontes@outlook.com.br>
2023-07-25 20:36:53 -0300

    First request test with ViaCEP

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,4 +1,5 @@
 import Address from "../models/address.js";
+import * as requestService from "../services/request-service.js";
 
 function State() {
   this.address = new Address();
@@ -33,6 +34,16 @@ export function init() {
 
 
   state.btnClear.addEventListener("click", handleBtnClearClick);
+  state.btnSave.addEventListener("click", handleBtnSaveClick);
+
+  
+}
+
+
+async function handleBtnSaveClick(event) {
+    event.preventDefault();
+    const result = await requestService.getJson('https://viacep.com.br/ws/01001000/json/');
+    console.log(result);
 }
 
 function handleInputNumberChange(event) {
