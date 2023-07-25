6123cc14479d4ed0bea2d0f077b8bb01ed4a6753
viniciusmontes <viniciusmontes@outlook.com.br>
2023-07-25 20:20:56 -0300

    Event handleBtnClearClick

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -29,7 +29,10 @@ export function init() {
   state.errorCep = document.querySelector('[data-error="cep"]');
   state.errorNumber = document.querySelector('[data-error="number"]');
 
-  state.inputNumber.addEventListener('change', handleInputNumberChange);
+  state.inputNumber.addEventListener("change", handleInputNumberChange);
+
+
+  state.btnClear.addEventListener("click", handleBtnClearClick);
 }
 
 function handleInputNumberChange(event) {
@@ -38,6 +41,23 @@ function handleInputNumberChange(event) {
   } else {
     setFormError("number", "");
   }
+}
+
+function handleBtnClearClick(event) {
+  event.preventDefault();
+  clearForm();
+}
+
+function clearForm() {
+  state.inputCep.value = "";
+  state.inputCity.value = "";
+  state.inputNumber.value = "";
+  state.inputStreet.value = "";
+
+  setFormError("cep", "");
+  setFormError("number", "");
+
+  state.inputCep.focus();
 }
 
 function setFormError(key, value) {
